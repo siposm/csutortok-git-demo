23fe6fed62b511e26007f700ac52a3e6d78c17c2
Sipos Miklos <commit@siposm.hu>
2024-10-08 19:28:34 +0200

    program logic extended

--- program.js
+++ program.js
@@ -1,3 +1,11 @@
-for (let i = 0; i < 10; i++) {
-    console.log('hello')
+console.log('Welcome to my program!')
+
+let number = 5
+
+if (number < 10) {
+    for (let i = 0; i < 10; i++) {
+        console.log(i+1 + ' hello')
+    }
+} else {
+    console.log('The number is bigger then 10! =(')
 }
